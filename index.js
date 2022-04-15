05f92d6be9d0c9af17995807992203184bd2cba8
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-04-15 17:41:38 +0430

    Rendering our First EJS Template

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const motors = require('./data/data.json');
 
+app.set('view engine', 'ejs')
+
 app.get('/' ,(req,res)=>{
-    res.send(motors);
+    res.render('shop');
 });
 
 app.get('/product' ,(req,res)=>{
 
     const motor = motors.find((mtr) => mtr.id === req.query.id);
-    res.send(motor);
+    res.render('product');
 });
 
 app.listen(3000 , () => console.log('server is running at 3000 port'));
