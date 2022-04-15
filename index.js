253d50fbdf6ab48612367e5dfbc08a12497107c1
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-04-15 17:29:04 +0430

    Server Setup with Declaring product page the Routes with Express

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
 const app = express();
-
+const motors = require('./data/data.json');
 
 app.get('/' ,(req,res)=>{
-   return res.send("<h1>wellcome to express motor shop</h1>");
-})
+    res.send(motors);
+});
+
+app.get('/product' ,(req,res)=>{
+
+    const motor = motors.find((mtr) => mtr.id === req.query.id);
+    res.send(motor);
+});
 
 app.listen(3000 , () => console.log('server is running at 3000 port'));
